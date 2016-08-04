696fd754e52a43631b679599f44e0519ecc19d64
Ben Anglin <forgetfulyoshi@gmail.com>
2016-08-03 22:33:19 -0400

    Add basic tests for Vec2 and direction-finding

--- flower/point.py
+++ flower/point.py
@@ -18,6 +18,11 @@ class Vec2(object):
         y = self.y - other.y
         return Vec2(x, y)
 
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vec2(x, y)
+
     def __xor__(self, other):
         cross = self.x * other.y - other.x * self.y
         return cross
--- flower/test/test_point.py
+++ flower/test/test_point.py
@@ -1,10 +1,51 @@
 import unittest
 
+from flower.point import Vec2
+from flower.point import direction
 
-class PointTests(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
 
+class Vec2Tests(unittest.TestCase):
+    def test_addition(self):
+        vec1 = Vec2(3, -5)
+        vec2 = Vec2(4, -2)
+        result = vec1 + vec2
+        expected = Vec2(7, -7)
+
+        self.assertEqual((expected.x, expected.y), (result.x, result.y))
+
+    def test_subtraction(self):
+        vec1 = Vec2(3, -5)
+        vec2 = Vec2(4, -2)
+        result = vec1 - vec2
+        expected = Vec2(-1, -3)
+
+        self.assertEqual((expected.x, expected.y), (result.x, result.y))
+
+    def test_cross_product(self):
+        vec1 = Vec2(3, -5)
+        vec2 = Vec2(4, -2)
+        result = vec1 ^ vec2
+        expected = 14  # WolframAlpha
+
+        self.assertEqual(expected, result)
+
+    def test_turns_left(self):
+        vec1 = Vec2(1, 1)
+        vec2 = vec1 + Vec2(0, 2)
+        turn = direction(Vec2(0, 0), vec1, vec2)
+        self.assertLess(turn, 0)
+
+    def test_turns_right(self):
+        vec1 = Vec2(1, 1)
+        vec2 = vec1 + Vec2(2, 0)
+        turn = direction(Vec2(0, 0), vec1, vec2)
+        self.assertGreater(turn, 0)
+
+    def test_turns_straight(self):
+        vec1 = Vec2(1, 1)
+        vec2 = vec1 + Vec2(1, 1)
+        turn = direction(Vec2(0, 0), vec1, vec2)
+        self.assertEqual(turn, 0)
 
 if __name__ == '__main__':
     unittest.main()
