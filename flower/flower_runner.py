Fix zero-segment anchor bug in buffer size calculation

flower/flower_runner.py
@@ -191,7 +191,9 @@ class FLOWERRunner(object):
             volume = self.energy_model.cluster_data_volume(
                 cluster, intercluster_only=True)
 
-            volume /= len(cluster.anchor.segments)
+            if len(cluster.anchor.segments) > 0:
+                volume /= len(cluster.anchor.segments)
+
             data_volumes.append(volume)
 
         max_data_volume = np.max(data_volumes) * pq.bit

flower/movement.py
@@ -45,7 +45,11 @@ class FLOWERMovementModel(object):
                     self._segment_indexes[seg] = i
                     i += 1
 
-        node_count = len(self.sim.cells) + 1
+        if self.sim.hub.cells == [self.sim.damaged]:
+            node_count = len(self.sim.cells) + 1
+        else:
+            node_count = len(self.sim.cells)
+
         g_sparse = np.zeros((node_count, node_count), dtype=float)
         g_sparse[:] = np.inf
 