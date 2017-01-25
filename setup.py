eb070beca5a417b798f7b88df99b7604e2cfe6cc
Ben Anglin <forgetfulyoshi@gmail.com>
2017-01-24 22:44:13 -0500

    Add a proper setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wsnsims',
+    version='2.0.0.dev1',
+    description='Simulations for various WSN federation algorithms',
+    long_description=long_description,
+    url='https://github.com/forgetfulyoshi/wsnsims',
+    author='Ben Anglin',
+    author_email='jama1@umbc.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+    ],
+    keywords='wsn federation flower focus minds tocs',
+    packages=find_packages(exclude=['doc', 'test']),
+    install_requires=[
+        'matplotlib',
+        'nose',
+        'numpy',
+        'pyclustering',
+        'quantities',
+        'scipy',
+    ],
+
+
+)
