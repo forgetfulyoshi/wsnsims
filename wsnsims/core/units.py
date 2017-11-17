eafed148ee70629e89e548df21619f0f0e8347ca
forgetfulyoshi <forgetfulyoshi@users.noreply.github.com>
2017-11-16 23:37:11 -0500

    Remove old custom units
    With the removal of the quanities package from use, we can
    also remove our custom units module.

--- wsnsims/core/units.py
+++
@@ -1,5 +0,0 @@
-import quantities as pq
-
-GB = pq.UnitQuantity('gigabyte', pq.gibi * pq.byte, symbol='GB')
-MB = pq.UnitQuantity('megabyte', pq.mebi * pq.byte, symbol='MB')
-KB = pq.UnitQuantity('kilobyte', pq.kibi * pq.byte, symbol='KB')
